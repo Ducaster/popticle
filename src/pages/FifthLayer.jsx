300a1cfb9a84f3f3adf8f9e70d67279e20aee04a
Higashino-Keigo <kooa3399@kakao.com>
2024-03-03 23:24:15 +0900

    사진 크기 수정

--- src/pages/FifthLayer.jsx
+++ src/pages/FifthLayer.jsx
@@ -8,7 +8,7 @@ const FifthLayerContainer = styled.div`
   background-image: url("/assets/review_pc.jpg");
   background-size: cover;
   background-position: center;
-  height: 5500px;
+  height: 6600px;
 
   // 태블릿 크기에 대한 미디어 쿼리
   @media (max-width: 960px) {
