3e805603027d0ede45773787486a36bb0dbbe41f
Higashino-Keigo <kooa3399@kakao.com>
2024-03-03 23:54:28 +0900

    사진 크기 수정

--- src/pages/SecondLayer.jsx
+++ src/pages/SecondLayer.jsx
@@ -5,19 +5,19 @@ export default function SecondLayer() {
 }
 
 const SecondLayerContainer = styled.div`
-  background-image: url("/assets/particle_pc.png");
+  background-image: url("/assets/particle_pc.jpg");
   background-size: cover;
   background-position: center;
   height: 100vh;
 
   // 태블릿 크기에 대한 미디어 쿼리
   @media (max-width: 960px) {
-    background-image: url("/assets/particle_tablet.png"); // 태블릿용 이미지
+    background-image: url("/assets/particle_tablet.jpg"); // 태블릿용 이미지
   }
 
   // 모바일 크기에 대한 미디어 쿼리
   @media (max-width: 480px) {
-    background-image: url("/assets/particle_phone.png"); // 모바일용 이미지
+    background-image: url("/assets/particle_phone.jpg"); // 모바일용 이미지
     height: 1270px;
   }
 `;
