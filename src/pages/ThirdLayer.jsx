e72ea529dc7773b9c87e7d4b2e6eb0396a088b3f
Higashino-Keigo <kooa3399@kakao.com>
2024-03-04 00:07:34 +0900

    사진 크기 수정

--- src/pages/ThirdLayer.jsx
+++ src/pages/ThirdLayer.jsx
@@ -18,7 +18,7 @@ const ThirdLayerContainer = styled.div`
 
   // 모바일 크기에 대한 미디어 쿼리
   @media (max-width: 480px) {
-    background-image: url("/assets/vision_phone.png"); // 모바일용 이미지
+    background-image: url("/assets/vision_phone.jpg"); // 모바일용 이미지
     height: 2921px;
   }
 `;
